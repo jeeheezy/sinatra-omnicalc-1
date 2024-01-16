b7b17a99d1f0751162696e96c2bb77f6e86fd0ea
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2024-01-16 22:07:17 +0000

    Committing updates for /square_root and its results

--- app.rb
+++ app.rb
@@ -9,9 +9,20 @@ get("/square/new") do
   erb(:new_square_calc)
 end
 
-get '/square/results' do
+get("/square/results") do
   @num = params["user_number"]
   @result = (@num.to_f) **2
 
   erb(:square_results)
+end
+  
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+  @num = params["user_number"]
+  @result = (@num.to_f) ** 0.5
+  
+  erb(:square_root_results)
 end

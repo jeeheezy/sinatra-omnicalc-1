c7c9d6324cdf39d96b4eba1714c0fa400f0dcd18
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2024-01-16 21:55:59 +0000

    Committing updates for /square and its results, which are working and look identical to target

--- app.rb
+++ app.rb
@@ -2,8 +2,16 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:new_square_calc)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get '/square/results' do
+  @num = params["user_number"]
+  @result = (@num.to_f) **2
+
+  erb(:square_results)
 end

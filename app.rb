402691280b12d5304bfeeb6537a07c1fa2ccd0a8
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2024-01-16 22:16:33 +0000

    Committing updates for /random and its results

--- app.rb
+++ app.rb
@@ -23,6 +23,18 @@ end
 get("/square_root/results") do
   @num = params["user_number"]
   @result = (@num.to_f) ** 0.5
-  
+
   erb(:square_root_results)
+end
+
+get("/random/new") do
+  erb(:new_random)
+end
+
+get("/random/results") do
+  @min = params["min_number"].to_f
+  @max = params["max_number"].to_f
+  @result = rand(@min..@max)
+
+  erb(:random_results)
 end
